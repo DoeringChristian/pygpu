1bddf0cd5ec85bea64576ecf15a2c4afb062a6e0
Doering Christian <christian.doering@tum.de>
2022-10-09 20:12:54 +0200

    Indent

--- glsl.py
+++ glsl.py
@@ -33,31 +33,63 @@ def glsl_type(type: type) -> str:
         raise Exception(f"Type not supported {type=}")
 
 
+def glsl_load(dst: ir.Id, set, binding) -> str:
+    type = ir.ir.type(dst)
+    if type == gen.FloatArray:
+        ret = f"layout(std430, set={set}, binding={binding}) buffer Buf{dst.idx}" + "{\n"
+        ret += f"    {glsl_type(type)} var{dst.idx}[];\n"
+        ret += "};\n\n"
+        return ret
+
+    pass
+
+
 def compile_to_glsl(src: ir.Ir) -> str:
     glsl = """
 #version 450
-    """
+"""
+
+    indentlvl = 0
+
+    def indent() -> str:
+        ret = ""
+        for i in range(indentlvl):
+            ret += "    "
+        return ret
 
     for idx, op in enumerate(src.ops):
         print(f"{idx}: {op}")
+
         match op:
             case ir.FnBegin():
+                glsl += indent()
                 glsl += "void fn" + f"{idx}" + "("
             case ir.FnBody():
+                glsl += indent()
                 glsl = glsl[:-1]  # TODO: find better way to remove comma
                 glsl += "){\n"
+                indentlvl += 1
             case ir.FnEnd():
                 glsl += "}\n"
+                indentlvl -= 1
             case ir.Arg(dst):
+                glsl += indent()
                 glsl += glsl_type(ir.ir.type(dst)) + f" var{dst.idx},"
             case ir.Const(dst, val):
+                glsl += indent()
                 glsl += glsl_type(ir.ir.type(dst)) + \
                     f" var{dst.idx}" + glsl_const_assignment(val) + ";\n"
             case ir.Add(lhs, rhs, dst):
+                glsl += indent()
                 glsl += f"{glsl_type(ir.ir.type(dst))} var{dst.idx} = var{lhs.idx} + var{rhs.idx};\n"
             case ir.IAdd(dst, rhs):
+                glsl += indent()
                 glsl += f"var{dst.idx} += var{rhs.idx};\n"
             case ir.GetItem(src, dst, idx):
+                glsl += indent()
                 glsl += f"{glsl_type(ir.ir.type(dst))} var{dst.idx} = var{src.idx}[var{idx.idx}];\n"
+            case ir.Load(dst, set, binding):
+                glsl += indent()
+                glsl += glsl_load(dst, set, binding)
 
     print(glsl)
--- test.py
+++ test.py
@@ -2,21 +2,19 @@ from gen import Var, Fn, Int, Float, FloatArray, precomp
 from ir import ir
 import glsl
 
+z = FloatArray(set=0, binding=0)
+
 
 @precomp
 def test(x: Int):
-    a = x + Int(10)
-    pass
-
-
-if __name__ == "__main__":
     arr = FloatArray([1., 2., 3.])
     x = arr[Int(0)]
     y = x + arr[Int(1)]
     y += arr[Int(2)]
+    pass
 
-    z = Int(set=0, binding=0)
 
+if __name__ == "__main__":
     # print(f"{ir=}")
 
     glsl.compile_to_glsl(ir)
